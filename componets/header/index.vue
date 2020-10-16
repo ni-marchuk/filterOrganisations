286b5110b0f53c9073754e05de80e039f04796c6
ni-marchuk <marchuknikita@gmail.com>
2020-10-16 22:01:56 +0300

    first commit

---
+++ componets/header/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="header">
+        <p>header</p>
+    </header>
+
+</template>
+
+<script>
+
+    export default {
+        name: "Header",
+
+    }
+</script>
+
+<style lang="scss">
+    .header {
+        &__top {
+            margin-bottom: 20px;
+        }
+
+        &__bottom {
+
+        }
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <p>body</p>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Index',
+    }
+
+</script>
+
+<style>
+    .container {
+        margin: 0 auto;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+</style>
